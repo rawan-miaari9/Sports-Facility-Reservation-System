237b701ac8c8096c5d4b2c5f6a584ba3f57a75d4
ghinwa927 <ghinwaabousahyoun9@gmail.com>
2026-07-23 14:17:47 +0300

    JWT added

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,15 +1,15 @@
-// app/api/auth/login/route.ts
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/db/mongodb";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
-import { loginSchema } from "../../../../validators/auth/auth";
+import { loginSchema } from "@/validators/auth/auth";
+import jwt from "jsonwebtoken";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
 
-    // Zod Validation
+    //  Zod Validation
     const validation = loginSchema.safeParse(body);
     if (!validation.success) {
       return NextResponse.json(
@@ -20,9 +20,10 @@ export async function POST(request: Request) {
 
     const { email, password } = validation.data;
 
+    //  Connect to Database
     await connectDB();
 
-    // find the user in database
+    //  check if user already in db
     const user = await User.findOne({ email });
     if (!user) {
       return NextResponse.json(
@@ -31,7 +32,7 @@ export async function POST(request: Request) {
       );
     }
 
-    // Compare password
+    //  Compare Password
     let isMatch = false;
     if (user.password.startsWith("$2a$") || user.password.startsWith("$2b$")) {
       isMatch = await bcrypt.compare(password, user.password);
@@ -46,9 +47,31 @@ export async function POST(request: Request) {
       );
     }
 
-    //Return user data (exclude password)
+    // Generate JWT Token (AFTER user and password are valid)
+    const jwtSecret = process.env.JWT_SECRET;
+    if (!jwtSecret) {
+      console.error("JWT_SECRET is not defined in environment variables");
+      return NextResponse.json(
+        { message: "Server authentication error" },
+        { status: 500 }
+      );
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email,
+        name:user.name,
+        role: user.role,
+      },
+      jwtSecret,
+      { expiresIn: "1d" }
+    );
+
+    // Return user data AND token
     return NextResponse.json({
       message: "Login successful",
+      token, 
       user: {
         id: user._id.toString(),
         name: user.name,
