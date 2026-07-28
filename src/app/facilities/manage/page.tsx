c9dae90e9ead761ead3e3ad6f9063924421de338
Mohammad Ahmad <mhmd.ahmad_04@outlook.com>
2026-07-28 14:09:28 +0300

    Restore facilities manage page

---
+++ src/app/facilities/manage/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import ManageFacilityView from '@/components/ManageFacilityVieww';
+
+export default function ManageFacilityPage() {
+  return (
+    <main className="min-h-screen bg-background">
+      <ManageFacilityView 
+        editingFacility={null} 
+        onSaveFacility={(facility) => {
+          console.log('Successfully saved facility:', facility);
+        }}
+        onCancel={() => {
+          console.log('Action cancelled');
+        }}
+      />
+    </main>
+  );
+}
